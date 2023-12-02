4ea0bdf03f23e0f67ac1251b722433c79001b52d
Osman Herman <osmanherman27@gmail.com>
2023-12-02 11:13:19 -0300

    parte 5.1

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
 'use strict';
 
-const ventajasMobile = document.querySelector("#ventajas .ventaja-mobile p");
-const ventajasIconos = document.querySelectorAll("#ventajas .icono");
-const ventajasTextos = document.querySelectorAll("#ventajas .ventaja p");
+// P R E L O A D E R
 
-// Caso inicial
-ventajasIconos[0].classList.add('seleccionado');
-
-// Header 
-const header = document.querySelector('header');
-const toggleBtn = document.querySelector('header .icons button');
-
-toggleBtn.addEventListener('click', () => {
-    header.classList.toggle('open');
-})
-
-// PRELOADER
 window.addEventListener("load", function () {
     var onloadHTML = document.getElementById('onload');
     var bodyHTML = document.getElementById('bodyAr');
@@ -23,6 +9,104 @@ window.addEventListener("load", function () {
     onloadHTML.style.display = 'none';
     bodyHTML.classList.remove('hidden');
 });
+
+
+
+// SECCION: VENTAJAS
+
+const ventajasMobile = document.querySelector("#ventajas .ventaja-mobile p");
+const ventajasIconos = document.querySelectorAll("#ventajas .icono");
+const ventajasTextos = document.querySelectorAll("#ventajas .ventaja p");
+
+ventajasIconos[0].classList.add('seleccionado');
+
+ventajasIconos.forEach((icono, index) => {
+    icono.addEventListener('click', ()=>{
+        ventajasIconos.forEach(icono => icono.classList.remove('seleccionado'));
+        ventajasIconos[index].classList.add('seleccionado');
+        ventajasMobile.textContent = ventajasTextos[index].textContent;
+    })
+})
+
+
+
+// H E A D E R
+
+const header = document.querySelector('header');
+const toggleBtn = document.querySelector('header .icons button');
+
+toggleBtn.addEventListener('click', () => {
+    header.classList.toggle('open');
+    document.getElementById('one').classList.toggle('oneClick')
+    document.getElementById('two').classList.toggle('twoClick')
+    document.getElementById('trhee').classList.toggle('trheeClick')
+})
+
+/*document.querySelector(".hamb").addEventListener('click', function(e) {
+    document.getElementById('one').classList.toggle('oneClick')
+    document.getElementById('two').classList.toggle('twoClick')
+    document.getElementById('trhee').classList.toggle('trheeClick')
+    document.querySelector('.menu').classList.toggle('menuClick')
+    document.querySelector('.navBar').classList.toggle('navBar-background')
+  })*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Panel de redes
+/* document.addEventListener("DOMContentLoaded", function() {
+    var anchoRedes = document.querySelector(".redes").offsetWidth;
+    var altoRedes = (document.querySelector(".redes").offsetHeight)/2;
+    var anchoScroll = (document.body.scrollWidth - window.innerWidth)*(-1);
+    document.querySelector(".redes").style.transform = `translate(calc(100vw - ${anchoRedes + anchoScroll}px), calc(50vh - ${altoRedes}px)`;
+})
+
+window.addEventListener('resize', () => {
+    var anchoRedes = document.querySelector(".redes").offsetWidth;
+    var altoRedes = (document.querySelector(".redes").offsetHeight)/2;
+    var anchoScroll = (document.body.scrollWidth - window.innerWidth)*(-1);
+    document.querySelector(".redes").style.transform = `translate(calc(100vw - ${anchoRedes + anchoScroll}px), calc(50vh - ${altoRedes}px)`;
+})
+ */
+
 
 /*window.addEventListener("load", function () {
     var onloadSelectorHTML = document.getElementById('onload');
@@ -47,28 +131,3 @@ window.addEventListener("load", function () {
     onloadHTML.style.display = 'none';
     bodyHTML.classList.remove('hiddenSelector');
 }*/
-
-
-// Agregado de evento a todos los iconos
-ventajasIconos.forEach((icono, index) => {
-    icono.addEventListener('click', ()=>{
-        ventajasIconos.forEach(icono => icono.classList.remove('seleccionado'));
-        ventajasIconos[index].classList.add('seleccionado');
-        ventajasMobile.textContent = ventajasTextos[index].textContent;
-    })
-})
-
-// Panel de redes
-document.addEventListener("DOMContentLoaded", function() {
-    var anchoRedes = document.querySelector(".redes").offsetWidth;
-    var altoRedes = (document.querySelector(".redes").offsetHeight)/2;
-    var anchoScroll = (document.body.scrollWidth - window.innerWidth)*(-1);
-    document.querySelector(".redes").style.transform = `translate(calc(100vw - ${anchoRedes + anchoScroll}px), calc(50vh - ${altoRedes}px)`;
-})
-
-window.addEventListener('resize', () => {
-    var anchoRedes = document.querySelector(".redes").offsetWidth;
-    var altoRedes = (document.querySelector(".redes").offsetHeight)/2;
-    var anchoScroll = (document.body.scrollWidth - window.innerWidth)*(-1);
-    document.querySelector(".redes").style.transform = `translate(calc(100vw - ${anchoRedes + anchoScroll}px), calc(50vh - ${altoRedes}px)`;
-})
